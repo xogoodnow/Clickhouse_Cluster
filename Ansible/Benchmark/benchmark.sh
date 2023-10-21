df395905168b62662f701df5a75648697ad958c5
xogoodnow <xo4ever9999@gmail.com>
2023-10-21 16:53:31 +0330

    Script checks if clickhouse-benchmark exists on the node or not

--- Ansible/Benchmark/benchmark.sh
+++ Ansible/Benchmark/benchmark.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Check if clickhouse-benchmark is installed
+if ! command -v clickhouse-benchmark &> /dev/null
+then
+    echo "clickhouse-benchmark could not be found, installing..."
+    sudo apt-get install -y apt-transport-https ca-certificates dirmngr
+    GNUPGHOME=$(mktemp -d)
+    sudo GNUPGHOME="$GNUPGHOME" gpg --no-default-keyring --keyring /usr/share/keyrings/clickhouse-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8919F6BD2B48D754
+    sudo rm -r "$GNUPGHOME"
+    sudo chmod +r /usr/share/keyrings/clickhouse-keyring.gpg
+
+    echo "deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] https://packages.clickhouse.com/deb stable main" | sudo tee \
+        /etc/apt/sources.list.d/clickhouse.list
+    sudo apt-get update
+    sudo apt-get install -y clickhouse-client
+fi
+
 # List of ClickHouse nodes
 nodes=("clickhouse-0" "clickhouse-1" "clickhouse-2")
 
@@ -14,10 +30,10 @@ query="SELECT Year, Variable_name, SUM(toFloat64(replaceAll(Value, ',', ''))) as
 echo "$query" > query.sql
 
 # Number of iterations and concurrency
-iterations=10
-concurrency=10
+iterations=10000
+concurrency=100
 delay=1
-confidence=0.95
+confidence=5
 
 # Loop over each node
 for node in "${nodes[@]}"
