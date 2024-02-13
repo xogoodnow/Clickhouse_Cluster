cca83885a80f2568eeb27706e6b817d21130f6d9
xogoodnow <xo4ever9999@gmail.com>
2024-02-13 22:33:24 +0330

    Changed the benchmark script

--- Administarion_Guide/Benchmark/benchmark.sh
+++ Administarion_Guide/Benchmark/benchmark.sh
@@ -17,14 +17,14 @@ then
 fi
 
 # List of ClickHouse nodes
-nodes=("clickhouse-0" "clickhouse-1" "clickhouse-2")
+nodes=("clickhouse-1" "clickhouse-2" "clickhouse-3")
 
 # Username and password
 username="username"
 password="password"
 
 # SQL query
-query="SELECT t1.Industry_name_NZSIOC, t1.Variable_name AS Variable1, t2.Variable_name AS Variable2, avg(toFloat32OrNull(replaceRegexpAll(t1.Value, ',', ''))) AS AvgValue1, avg(toFloat32OrNull(replaceRegexpAll(t2.Value, ',', ''))) AS AvgValue2, arrayJoin(splitByString(' ', t1.Industry_code_ANZSIC06)) AS SplitIndustryCode, rank() OVER (ORDER BY avg(toFloat32OrNull(replaceRegexpAll(t1.Value, ',', ''))) DESC) AS RankByValue1, rank() OVER (ORDER BY avg(toFloat32OrNull(replaceRegexpAll(t2.Value, ',', ''))) DESC) AS RankByValue2 FROM kang.kangtable AS t1 JOIN kang.kangtable AS t2 ON t1.Industry_name_NZSIOC = t2.Industry_name_NZSIOC WHERE t1.Year = 2021 AND t2.Year = 2021 AND t1.Variable_name = 'Total income' AND t2.Variable_name = 'Sales, government funding, grants and subsidies' GROUP BY t1.Industry_name_NZSIOC, t1.Variable_name, t2.Variable_name, arrayJoin(splitByString(' ', t1.Industry_code_ANZSIC06)) ORDER BY RankByValue1, RankByValue2 LIMIT 10;"
+query="SELECT t1.Industry_name_NZSIOC, t1.Variable_name AS Variable1, t2.Variable_name AS Variable2, avg(toFloat32OrNull(replaceRegexpAll(t1.Value, ',', ''))) AS AvgValue1, avg(toFloat32OrNull(replaceRegexpAll(t2.Value, ',', ''))) AS AvgValue2, arrayJoin(splitByString(' ', t1.Industry_code_ANZSIC06)) AS SplitIndustryCode, rank() OVER (ORDER BY avg(toFloat32OrNull(replaceRegexpAll(t1.Value, ',', ''))) DESC) AS RankByValue1, rank() OVER (ORDER BY avg(toFloat32OrNull(replaceRegexpAll(t2.Value, ',', ''))) DESC) AS RankByValue2 FROM click.clicktable AS t1 JOIN click.clicktable AS t2 ON t1.Industry_name_NZSIOC = t2.Industry_name_NZSIOC WHERE t1.Year = 2021 AND t2.Year = 2021 AND t1.Variable_name = 'Total income' AND t2.Variable_name = 'Sales, government funding, grants and subsidies' GROUP BY t1.Industry_name_NZSIOC, t1.Variable_name, t2.Variable_name, arrayJoin(splitByString(' ', t1.Industry_code_ANZSIC06)) ORDER BY RankByValue1, RankByValue2 LIMIT 10;"
 
 # Save the query to a file
 echo "$query" > query.sql
