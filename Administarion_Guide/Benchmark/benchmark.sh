7dd3667686eb095f5107622f27905b5f57ff1985
xogoodnow <xo4ever9999@gmail.com>
2024-02-19 14:09:40 +0330

    Fixed the issues with the bench script

--- Administarion_Guide/Benchmark/benchmark.sh
+++ Administarion_Guide/Benchmark/benchmark.sh
@@ -25,7 +25,6 @@ password="password"
 
 # SQL query
 query="SELECT t1.Industry_name_NZSIOC, t1.Variable_name AS Variable1, t2.Variable_name AS Variable2, avg(toFloat32OrNull(replaceRegexpAll(t1.Value, ',', ''))) AS AvgValue1, avg(toFloat32OrNull(replaceRegexpAll(t2.Value, ',', ''))) AS AvgValue2, arrayJoin(splitByString(' ', t1.Industry_code_ANZSIC06)) AS SplitIndustryCode, rank() OVER (ORDER BY avg(toFloat32OrNull(replaceRegexpAll(t1.Value, ',', ''))) DESC) AS RankByValue1, rank() OVER (ORDER BY avg(toFloat32OrNull(replaceRegexpAll(t2.Value, ',', ''))) DESC) AS RankByValue2 FROM click.clicktable AS t1 JOIN click.clicktable AS t2 ON t1.Industry_name_NZSIOC = t2.Industry_name_NZSIOC WHERE t1.Year = 2021 AND t2.Year = 2021 AND t1.Variable_name = 'Total income' AND t2.Variable_name = 'Sales, government funding, grants and subsidies' GROUP BY t1.Industry_name_NZSIOC, t1.Variable_name, t2.Variable_name, arrayJoin(splitByString(' ', t1.Industry_code_ANZSIC06)) ORDER BY RankByValue1, RankByValue2 LIMIT 10;"
-
 # Save the query to a file
 echo "$query" > query.sql
 
@@ -41,7 +40,7 @@ do
   echo "Benchmarking node: $node"
 
   # Run benchmark
-  clickhouse-benchmark -i $iterations -c $concurrency --delay $delay --confidence $confidence --host=$node --user=$username --password=$password < query.sql
+  clickhouse-benchmark --secure -i $iterations -c $concurrency --delay $delay --confidence $confidence --host=$node --user=$username --password=$password < query.sql
 done
 
 # Remove the query file
