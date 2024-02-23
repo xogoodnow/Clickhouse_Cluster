809ad5dbd42be9dd3ad11b3b1fec8202866e05ed
xogoodnow <xo4ever9999@gmail.com>
2024-02-23 16:22:31 +0330

    Changed the command for ansible

--- Terraform/modules/Clickhouse/main.tf
+++ Terraform/modules/Clickhouse/main.tf
@@ -1,7 +1,7 @@
 
 resource "null_resource" "Clickhouse" {
   provisioner "local-exec" {
-    command = "sleep 111  && PWD='../' ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yaml ../Ansible/playbooks/Setup.yaml --private-key sshkey/private_key.pem"
+    command = "sleep 111  && PWD='../' ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH='../Ansible/roles/' ansible-playbook -i ../Ansible/inventory ../Ansible/playbooks/Deploy.yaml --private-key sshkey/private_key.pem"
   }
 }
 
