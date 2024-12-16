178e8c0577e10dacc2c53369f41d107146463032
Raman Vahedi <ramanvhd@gmail.com>
2024-12-17 01:02:44 +0330

    Bump hetzner provider version (1.42.0 -> 1.49.1)

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     hcloud = {
-      source = "hetznercloud/hcloud"
-      version = "1.42.0"
+      source  = "hetznercloud/hcloud"
+      version = "1.49.1"
     }
   }
 }
